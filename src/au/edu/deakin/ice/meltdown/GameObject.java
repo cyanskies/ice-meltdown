acb3eb1abc63c9961f75363859da6a85468f134c
stevenjpilkington@gmail.com <cyanskies@users.noreply.github.com>
2012-08-11 05:56:25 +0000

    Addeed classes Vector2 and rect to aid in movement of game objects and calculating collisions.

--- src/au/edu/deakin/ice/meltdown/GameObject.java
+++ src/au/edu/deakin/ice/meltdown/GameObject.java
@@ -4,23 +4,23 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.util.Log;
 
 public class GameObject{
 
-	private static Resources mR;
+	private static Resources mR = null;
 	public static void setResources(Resources r){
 		mR = r;
 	}
 	
 	private static final String mName = GameObject.class.getSimpleName();
-	private Bitmap mB;
 	
+	private Bitmap mB;
 	public Bitmap getBitmap() {
 		return mB;
 	}
 
 	private Matrix mM = new Matrix();
-
 	public Matrix getMatrix() {
 		return mM;
 	}
@@ -28,7 +28,10 @@ public class GameObject{
 
 	public GameObject(int image){
 		if(mR == null)
+		{
+			Log.d(mName, "Someone forgot to initialise GameObject.Resources");
 			throw new NullPointerException();
+		}
 		mB = BitmapFactory.decodeResource(mR, image);
 		mM.reset();
 		// TODO create drawable using constructor parameters
