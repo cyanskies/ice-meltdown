825afe4847350d1c6b4a7db5ddce882f0384b8d7
stevenjpilkington@gmail.com <cyanskies@users.noreply.github.com>
2012-08-12 05:25:14 +0000

    Resized ground.png and positioned it correctly on the screen.

--- src/au/edu/deakin/ice/meltdown/IceGameView.java
+++ src/au/edu/deakin/ice/meltdown/IceGameView.java
@@ -2,32 +2,34 @@ package au.edu.deakin.ice.meltdown;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
 
 public class IceGameView extends GameView {
 
 	private static final String mName = IceGameView.class.getSimpleName();
-	private final GameObject test = new GameObject(R.drawable.ic_launcher);
+	private final GameObject mSnowman = new GameObject(R.drawable.ic_launcher);
 	private final GameObject mGround = new GameObject(R.drawable.ground);
-        private final GameObject mSnowman = new GameObject(R.drawable.ground);
 	
 	public IceGameView(Context context) {
 		super(context);
-		test.move(50, 50);
-		mGround.move(100, 100);
+		Log.d(mName, "Creating gameview");
+		mSnowman.move(50, 550);
+		mGround.move(0, 600);
 		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void Update(){
-		
+		Log.d(mName, "Starting Update step");
 	}
 	
 	@Override
 	public void Draw(Canvas canvas){
+		Log.d(mName, "Starting Draw step");
 		// clear, go through each entity and call draw, then call display
 		clear(canvas);
 		
-		draw(test);
+		draw(mSnowman);
 		draw(mGround);
 		
 		display(canvas);
