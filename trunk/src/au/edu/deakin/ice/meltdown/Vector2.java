7d897fb9328db69f7b82d1fe8a552ae23f1f9c46
cmjd22@gmail.com <cmjd22@gmail.com@55bbe62a-4315-49ec-46cd-62d5a5ab4c57>
2012-08-16 12:09:08 +0000

    Added javadoc comments to the Vector2 class.

--- trunk/src/au/edu/deakin/ice/meltdown/Vector2.java
+++ trunk/src/au/edu/deakin/ice/meltdown/Vector2.java
@@ -1,29 +1,43 @@
 package au.edu.deakin.ice.meltdown;
 
+/** The Vector2 class is used to hold 2d values*/
 public class Vector2 {
 
+	/** A position value of the current Vector2 */
 	public float x = 0.f, y = 0.f;
 	
+	/** Vector2 auto-generated constructor */
 	public Vector2() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/** Vector2 copy constructor 
+	 *  @param other The Vector2 object to copy
+	 *  */
 	public Vector2(Vector2 other) {
 		x = other.x;
 		y = other.y;
 	}
 	
+	/** Vector2 constructor
+	 * @param x The x value of the vector
+	 * @param y The y value of the vector */
 	public Vector2(float x, float y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+	/** The function used to add a Vector2 to the current vector2
+	 * @param v The Vector2 to add to the current vector2
+	 * @return The new Vector2 value */
 	public Vector2 add(Vector2 v){
 		this.x += v.x;
 		this.x += v.y;
 		return new Vector2(this);
 	}
 	
+	/** The function used to subtract a Vector2 from the current vector2
+	 * @param v The Vector2 to subtract from the current vector2
+	 * @return The new Vector2 value */
 	public Vector2 sub(Vector2 v){
 		this.x -= v.x;
 		this.x -= v.y;
