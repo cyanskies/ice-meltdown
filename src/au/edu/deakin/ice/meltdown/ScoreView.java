aaf3acdbebfbaa9a0f103bfda2026f4a937e3e7e
stevenjpilkington@gmail.com <cyanskies@users.noreply.github.com>
2012-09-13 11:42:05 +0000

    Scores are now displayed on the main menu.
    They are also persevered between game instances.
    
    This commit makes the game feature complete based on the latest version of our group's design documentation and proposals. Further commits from here will only consist of bug fixing, value tweeking and poilishing.

--- src/au/edu/deakin/ice/meltdown/ScoreView.java
+++ src/au/edu/deakin/ice/meltdown/ScoreView.java
@@ -1,5 +1,6 @@
 package au.edu.deakin.ice.meltdown;
 
+import android.R.color;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.Log;
@@ -12,6 +13,8 @@ public class ScoreView extends GameView {
 	private static final String mName = ScoreView.class.getSimpleName();
 	private GameObject mPlay, mExit;
 	
+	private TextObject[] mScores, mDates;
+	
 	public ScoreView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -22,21 +25,46 @@ public class ScoreView extends GameView {
 		mPlay = new GameObject(R.drawable.button_play);
 		mExit = new GameObject(R.drawable.button_exit);	
 		
-		mPlay.setPosition(new Vector2(50, 10));
-		mExit.setPosition(new Vector2(50, 70));
+		int buttonx = (int) (mScreenSize.x / 10);
+		int buttonHeight = (int) mPlay.getBounds().size.y;
+		
+		mPlay.setPosition(new Vector2(buttonx, buttonHeight));
+		mExit.setPosition(new Vector2(buttonx, buttonHeight * 3));
+		
+		int scorex = (int) (mScreenSize.x / 4);
+		int scorey = (int) (mScreenSize.y / 6);
+		int scoreStep = scorey / 5;
+		
+		MainActivity main = (MainActivity) mParent;
+		int[] scores = main.getScores();
+		String[] dates = main.getScoreDates();
+		
+		mScores = new TextObject[5];
+		mDates = new TextObject[5];
+		
+		for(int i = 0; i < scores.length; ++i){
+			mScores[i] = new TextObject(Integer.toString(scores[i]), color.primary_text_light);
+			mScores[i].setPosition(scorex * 2, scorey * i + scoreStep);
+		}
+		
+		for(int i = 0; i < dates.length; ++i){
+			mDates[i] = new TextObject(dates[i], color.primary_text_light);
+			mDates[i].setPosition(scorex * 3, scorey * i + scoreStep);
+		}
 	}
-			
-	//@Override
-	public void Update(){
-				
-	}
-			
+					
 	//@Override
 	public void Draw(Canvas canvas){
 		clear(canvas);
 		
 		draw(mPlay);
 		draw(mExit);
+		
+		for(TextObject o : mScores)
+			draw(o);
+		
+		for(TextObject o : mDates)
+			draw(o);
 		
 		display(canvas);
 	}
