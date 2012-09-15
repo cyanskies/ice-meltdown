eaa48d8fb813a95ab1a85d8be0b586f4cbf061c4
stevenjpilkington@gmail.com <cyanskies@users.noreply.github.com>
2012-09-15 06:21:37 +0000

    Added javadoc to DrawData, GameObject, GameThread, GameView and touched up the docs for MainActivity

--- src/au/edu/deakin/ice/meltdown/MainActivity.java
+++ src/au/edu/deakin/ice/meltdown/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.app.Activity;
 
+/** The main app controller, control's program flow a life cycle */
 public class MainActivity extends Activity{
 	
 	/** Class name used for logging */
